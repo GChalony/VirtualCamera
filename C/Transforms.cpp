fac394a66043ce9f167d632e35381e707cf9a184
gregnix <gchalony@gmail.com>
2020-11-29 18:38:33 +0100

    Add Matrix and Deformation transforms

--- C/Transforms.cpp
+++ C/Transforms.cpp
@@ -1,9 +1,11 @@
-#include <string.h>
+#include <string>
+#include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
+#include <cmath>
 #include "Transforms.h"
 
 using namespace cv;
@@ -28,18 +30,100 @@ uchar *WebcamCameraOutput::nextData(){
         cerr << "ERROR! blank frame grabbed\n";
         return NULL;
     }
+    transform(frame);
     return frame.data;
 }
+
+void WebcamCameraOutput::transform(Mat &img){}
 
 WebcamCameraOutput::~WebcamCameraOutput(){
     cap.release();
     frame.release();
 }
 
+CannyCameraOutput::CannyCameraOutput(int threshold1, int threshold2){
+    th1 = threshold1;
+    th2 = threshold2;
+}
+
+void CannyCameraOutput::transform(Mat &img){
+    cvtColor(img, gray, COLOR_RGB2GRAY);
+    Canny(gray, gray, th1, th2);
+    cvtColor(gray, img, COLOR_GRAY2RGB);
+}
+
+CannyCameraOutput::~CannyCameraOutput(){
+    gray.release();
+}
+
+MatrixCameraOutput::MatrixCameraOutput():
+chars("    .,:;oap!i1I)(}{OQXY#MW&8B"),
+downscale_factor(10)
+{
+    size = Size(WIDTH / downscale_factor, HEIGHT / downscale_factor);
+}
+
+
+void MatrixCameraOutput::transform(Mat& frame)
+{
+    Mat gray;
+    cvtColor(frame, gray, COLOR_BGR2GRAY);
+    resize(gray, gray, size);
+    double ratio = ((double)chars.size()) / 255;
+    gray = gray * ratio;
+    frame = Mat::zeros(Size(WIDTH, HEIGHT), CV_8UC3);
+    string c;
+    uint8_t val;
+    Scalar green = Scalar(0,255,0);
+    cout.flush();
+    for (int row=0; row<gray.rows; row++){
+        for (int col=0; col<gray.cols; col++){
+            val = gray.at<uint8_t>(row, col);
+            c = chars[val];
+            putText(frame, c, Point(col * downscale_factor, row * downscale_factor), 
+                    FONT_HERSHEY_DUPLEX, 0.4, green, 1
+            );
+        }
+    }
+}
+
+MatrixCameraOutput::~MatrixCameraOutput(){}
+
+
+DeformationMirrorCameraOutput::DeformationMirrorCameraOutput(float p, int r = HEIGHT / 4, int cx = HEIGHT / 2, int cy = WIDTH / 2){
+    cout << "Initiated with r=" << r << '\n';
+    map_X = Mat(HEIGHT, WIDTH, CV_32FC1);
+    map_Y = Mat(HEIGHT, WIDTH, CV_32FC1);
+    int d;
+    for (int i=0; i<HEIGHT; i++){
+        for (int j=0; j<WIDTH; j++){
+            d = (i - cx) * (i - cx) + (j - cy) * (j - cy);
+            // cout << "d=" << d << " vs " << r*r << '\n';
+
+            if (d < (r * r)){
+                // cout << i << " vs " << sign<int>(j - cy) * pow(((double)abs(j - cy)) / r, p) * r + cy;
+                map_X.at<float>(i, j) = sign<int>(j - cy) * pow(((double)abs(j - cy)) / r, p) * r + cy;
+                map_Y.at<float>(i, j) = sign<int>(i - cx) * pow(((double)abs(i - cx)) / r, p) * r + cx;
+            }
+            else{
+                map_X.at<float>(i, j) = j;
+                map_Y.at<float>(i, j) = i;
+            }
+        }
+    }
+    // cout << "map_X" << map_X << '\n';
+}
+
+void DeformationMirrorCameraOutput::transform(Mat& frame){
+    remap(frame, frame, map_X, map_Y, INTER_LINEAR);
+}
+
+DeformationMirrorCameraOutput::~DeformationMirrorCameraOutput(){}
+
 StaticImgCameraOutput::StaticImgCameraOutput(const string &imgPath, int width, int height){
     size = Size(width, height);
-    Mat imgInput = cv::imread(imgPath, cv::IMREAD_COLOR);
-    cv::resize(imgInput, img, size);
+    Mat imgInput = imread(imgPath, IMREAD_COLOR);
+    resize(imgInput, img, size);
 }
 
 uchar *StaticImgCameraOutput::nextData(){
@@ -59,7 +143,6 @@ StaticVideoCameraOutput::StaticVideoCameraOutput(const string &videoPath, int wi
     }
 
     n_frames = video.get(CAP_PROP_FRAME_COUNT);
-    cout << "Number of frames: " << n_frames << endl;
 }
 
 uchar *StaticVideoCameraOutput::nextData(){
@@ -75,7 +158,7 @@ uchar *StaticVideoCameraOutput::nextData(){
         cerr << "ERROR! blank frame grabbed\n";
         return NULL;
     }
-    cv::resize(buffer, frame, size);
+    resize(buffer, frame, size);
     i++;
     return frame.data;
 }
--- C/Transforms.h
+++ C/Transforms.h
@@ -1,10 +1,18 @@
+#ifndef TRANSFORM
+#define TRANSFORM
 #include <string.h>
 #include <opencv2/videoio.hpp>
+
+// Camera resolution
+#define HEIGHT 480 
+#define WIDTH 640
+#define BYTES WIDTH * HEIGHT * 3
 
 class CameraOutput
 {
     public:
         virtual uchar *nextData();
+        virtual ~CameraOutput() {};
 };
 
 class WebcamCameraOutput: public CameraOutput
@@ -12,12 +20,54 @@ class WebcamCameraOutput: public CameraOutput
     public:
         WebcamCameraOutput();
         uchar *nextData();
-        void transform();
-        ~WebcamCameraOutput();
+        /* Transform Webcam input inplace.*/
+        virtual void transform(cv::Mat &img);
+        virtual ~WebcamCameraOutput();
     
     private:
         cv::VideoCapture cap;
         cv::Mat frame;
+};
+
+class CannyCameraOutput: public WebcamCameraOutput
+{
+    public:
+        CannyCameraOutput(int, int);
+        void transform(cv::Mat&);
+        ~CannyCameraOutput();
+        
+    private:
+        cv::Mat gray;
+        int th1, th2;
+};
+
+
+class MatrixCameraOutput: public WebcamCameraOutput
+{
+    public:
+        MatrixCameraOutput();
+        void transform(cv::Mat&);
+        ~MatrixCameraOutput();
+    
+    private:
+        std::string chars;
+        int downscale_factor;
+        cv::Size size;
+};
+
+template <typename T> int sign(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
+class DeformationMirrorCameraOutput: public WebcamCameraOutput
+{
+    public:
+        DeformationMirrorCameraOutput(float p, int r, int cx, int cy);
+        void transform(cv::Mat&);
+        ~DeformationMirrorCameraOutput();
+    
+    private:
+        cv::Mat map_X, map_Y;
 };
 
 class StaticImgCameraOutput: public CameraOutput
@@ -47,3 +97,4 @@ class StaticVideoCameraOutput: public CameraOutput
         int n_frames;
 };
 
+#endif
